6a6a944810eecf9180bfa012a671e341fe14c053
Lakshmithra <lakshmithraramesh2007@gmail.com>
2025-06-29 14:18:57 +0530

    Create hello.cpp

---
+++ Basics/hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>                                          // This is a header file that allows input and output (I/O) using streams (cin and cout)
+
+int main() {                                             // Main function - the starting point of every C++ program
+    std::cout << "Hello World !";                       // Output statement that prints "Hello World !" to the screen
+    return 0;                                          // Ends the program and returns 0 to the operating system (means successful execution)
+}
